af0dcc479d6779cc4380122cc29467817cd8e6bd
rubystudent <rubystudent@mail.ru>
2021-09-07 21:14:18 +0300

    symbols of suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
-values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
-puts "#{values.sample} of #{suits.sample}"
+values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"]
+
+puts "#{values.sample}#{suits.sample}"
